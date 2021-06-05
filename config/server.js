9562828240e02ab90cd684e4d70376aeb8cf1a31
pcollinsTech <phil@pcollins.tech>
2021-06-05 12:07:56 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39d64827c3efda81640730713ae0a945'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '15f25615-61be-4fcf-a01f-4faab333b9a1'
+};
